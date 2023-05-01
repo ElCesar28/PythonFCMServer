12c193d79069c140fa98e0c3580adbf0940856fa
César Antonio Navarro Sosa <cesaaar26@gmail.com>
2023-05-01 00:28:55 -0600

    Initial commit

---
+++ SendPush.py
@@ -0,0 +1,4 @@
+import FCMManager as fcm
+
+tokens = ["e1ueaZd4R5WHX9G9ZQT_mR:APA91bFE7z6f83p4srXVkpV4cX2oyj2-e4V4lsElNvnMtLPgMkufIV-e5NzwcyYtauZh6BLofPCGUgxBgWRa9gS55-OBcgNw_M9qpLW6QuvKIkT6GRRiO2_X1YPwsTaE2aoN72gUnhHX"]
+fcm.sendPush("Ya quedo", "Hola desde python", tokens)
